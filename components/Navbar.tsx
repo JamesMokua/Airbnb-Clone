6710c0b627657a29357c04402efb50db4d757215
JamesMokua <jamieedgar02@gmail.com>
2023-10-11 18:08:08 +0300

    Navbar UI done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,14 @@
 import React from "react";
-import {
-  Menubar,
-  MenubarCheckboxItem,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
 import Image from "next/image";
 import { TbWorld } from "react-icons/tb";
 import { HiMiniBars3 } from "react-icons/hi2";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Search from "./Search";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex flex-row justify-between pt-4 items-center">
-      <div className="logo">
+    <div className="fixed w-full flex flex-row justify-between px-10 pt-5  items-center pb-3 bg-white shadow-sm border-b-[1px]">
+      <div className="logo hover:cursor-pointer">
         <Image
           src="/images/logo/logo.png"
           width="100"
@@ -30,89 +16,15 @@ const Navbar = () => {
           alt="logo"
         />
       </div>
-      <div className="pl-28">
-        <Menubar className="border border-slate-500 rounded-full px-4">
-          <MenubarMenu>
-            <MenubarTrigger>Anywhere</MenubarTrigger>
-            <MenubarContent>
-              <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>
-                New Window <MenubarShortcut>⌘N</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem disabled>New Incognito Window</MenubarItem>
-              <MenubarSeparator />
-              <MenubarSub>
-                <MenubarSubTrigger>Share</MenubarSubTrigger>
-                <MenubarSubContent>
-                  <MenubarItem>Email link</MenubarItem>
-                  <MenubarItem>Messages</MenubarItem>
-                  <MenubarItem>Notes</MenubarItem>
-                </MenubarSubContent>
-              </MenubarSub>
-              <MenubarSeparator />
-              <MenubarItem>
-                Print... <MenubarShortcut>⌘P</MenubarShortcut>
-              </MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger>Any week</MenubarTrigger>
-            <MenubarContent>
-              <MenubarItem>
-                Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>
-                Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-              </MenubarItem>
-              <MenubarSeparator />
-              <MenubarSub>
-                <MenubarSubTrigger>Find</MenubarSubTrigger>
-                <MenubarSubContent>
-                  <MenubarItem>Search the web</MenubarItem>
-                  <MenubarSeparator />
-                  <MenubarItem>Find...</MenubarItem>
-                  <MenubarItem>Find Next</MenubarItem>
-                  <MenubarItem>Find Previous</MenubarItem>
-                </MenubarSubContent>
-              </MenubarSub>
-              <MenubarSeparator />
-              <MenubarItem>Cut</MenubarItem>
-              <MenubarItem>Copy</MenubarItem>
-              <MenubarItem>Paste</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger>Add guests</MenubarTrigger>
-            <MenubarContent>
-              <MenubarCheckboxItem>
-                Always Show Bookmarks Bar
-              </MenubarCheckboxItem>
-              <MenubarCheckboxItem checked>
-                Always Show Full URLs
-              </MenubarCheckboxItem>
-              <MenubarSeparator />
-              <MenubarItem inset>
-                Reload <MenubarShortcut>⌘R</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem disabled inset>
-                Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
-              </MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem inset>Toggle Fullscreen</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem inset>Hide Sidebar</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
-      </div>
+      <Search />
       <div className="flex flex-row items-center">
-        <div className="pr-6   text-sm">Airbnb your home</div>
-        <div className="pr-3">
+        <div className="pr-6   text-sm hover:cursor-pointer">
+          Airbnb your home
+        </div>
+        <div className="pr-3 hover:cursor-pointer">
           <TbWorld />
         </div>
-        <div className="flex flex-row items-center border mr-4 px-1 py-1 border-slate-500 rounded-full">
+        <div className="flex flex-row items-center border mr-4 px-1 py-1 border-gray-300 rounded-full shadow-md hover:shadow-xl hover: cursor-pointer">
           <div className="pr-3 pl-1">
             <HiMiniBars3 />
           </div>
